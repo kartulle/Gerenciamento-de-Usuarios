a7d484ef8484521387d41eb1735d3c1cfa1f280d
Yago Andrade <contact@yagoandrade.com>
2023-08-23 14:33:33 -0300

    update routes

--- api/routes/main.go
+++ api/routes/main.go
@@ -2,15 +2,26 @@ package routes
 
 import (
 	controllers "api/api/controllers"
+
 	"github.com/gin-gonic/gin"
 )
 
 func AppRoutes(router *gin.Engine) *gin.RouterGroup {
 	userController := controllers.NewUserController()
 
-	v1:= router.Group("/v1")
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Essa é a página principal. Hello world!",
+		})
+	})
+
+	v1 := router.Group("/v1")
 	{
-		v1.GET("/users", userController.FindAll)
+		v1.GET("/users", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "Essa é a página para pegar todos os usuários e retornar via JSON",
+			})
+		}, userController.FindAll)
 	}
 
 	return v1
--- main.go
+++ main.go
@@ -1,46 +1,15 @@
 package main
 
 import (
-
 	routes "api/api/routes"
-    
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    app:= gin.Default()
+	app := gin.Default()
 
-    routes.AppRoutes(app)
-    
-    app.Run("localhost:3001")
+	routes.AppRoutes(app)
+
+	app.Run("localhost:3001")
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// func main() {
-//     fmt.Println("Hello, World!")
-// }
-// import(
-// 	"fmt"
-// 	"net/http"
-// )
-
-// func mainHandler(w http.ResponseWriter, r *http.Request){
-// 	fmt.Println(w, "Bem-vindo à API em Go!")
-// }
-
-// func main(){
-// 	http.HadleFunc("/", mainHandler)
-
-// 	fmt.Println("Servidor rodando em http://localhost:8080")
-// 	http.ListenAndServe(":8080", nill)
-// }
